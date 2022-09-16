b2617ec98448f082b9b57c28fe9fe8a951da0f50
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-09-16 11:28:41 +0100

    flake

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,3 @@
-import sys
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
@@ -7,7 +6,8 @@ setup(
     description="A library for the evaluation of finite element basis functions.",
     version="0.1.0",
     url="https://github.com/rusty-fast-solvers/rusty-element",
-    rust_extensions=[RustExtension("rusty_element._rusty_element", binding=Binding.PyO3, path="../rust/Cargo.toml")],
+    rust_extensions=[RustExtension(
+        "rusty_element._rusty_element", binding=Binding.PyO3, path="../rust/Cargo.toml")],
     packages=["rusty_element"],
     zip_safe=False,
 )
