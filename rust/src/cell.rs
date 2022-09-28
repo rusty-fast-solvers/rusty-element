c7aa717851714e9d41b5d9393aac9cf8bfc92d14
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-09-28 11:49:12 +0100

    style

--- rust/src/cell.rs
+++ rust/src/cell.rs
@@ -1,5 +1,8 @@
 //! Cell definitions
 
+pub mod triangle;
+pub use triangle::*;
+
 /// A 0- to 3- dimensional reference cell
 pub trait ReferenceCell {
     const DIM: usize;
@@ -40,7 +43,7 @@ pub trait ReferenceCell {
     fn volume_count(&self) -> usize;
 
     /// Get the entities connected to an entity
-    /// 
+    ///
     /// This function returns a list of entity numbers of entities of dimension CONNECTED_DIM that are attached to the entity numbered ENTITY_DIM of number entity_number.
     /// For example connectivity<1, 2>(0) will return a list of faces (2D entities) that are connected to edge (1D entity) 0.
     fn connectivity<const ENTITY_DIM: usize, const CONNECTED_DIM: usize>(
@@ -51,84 +54,6 @@ pub trait ReferenceCell {
 
 /// The reference interval
 struct Interval;
-
-/// The reference triangle
-pub struct Triangle;
-
-impl ReferenceCell for Triangle {
-    const DIM: usize = 2;
-    fn vertices(&self) -> &[f64] {
-        static VERTICES: [f64; 6] = [0.0, 0.0, 1.0, 0.0, 0.0, 1.0];
-        &VERTICES
-    }
-
-    fn edges(&self) -> &[usize] {
-        static EDGES: [usize; 6] = [1, 2, 0, 2, 0, 1];
-        &EDGES
-    }
-
-    fn faces(&self) -> &[usize] {
-        static FACES: [usize; 3] = [0, 1, 2];
-        &FACES
-    }
-    fn faces_nvertices(&self) -> &[usize] {
-        static FACES_NV: [usize; 1] = [3];
-        &FACES_NV
-    }
-
-    fn vertex_count(&self) -> usize {
-        3
-    }
-    fn edge_count(&self) -> usize {
-        3
-    }
-    fn face_count(&self) -> usize {
-        1
-    }
-    fn volume_count(&self) -> usize {
-        0
-    }
-    fn connectivity<const ENTITY_DIM: usize, const CONNECTED_DIM: usize>(
-        &self,
-        entity_number: usize,
-    ) -> Result<Vec<usize>, ()> {
-        match ENTITY_DIM {
-            0 => {
-                assert!(entity_number < 3);
-                match CONNECTED_DIM {
-                    0 => Ok(vec![entity_number]),
-                    1 => match entity_number {
-                        0 => Ok(vec![1, 2]),
-                        1 => Ok(vec![0, 2]),
-                        2 => Ok(vec![0, 1]),
-                        _ => Err(()),
-                    },
-                    2 => Ok(vec![0]),
-                    _ => Err(()),
-                }
-            }
-            1 => {
-                assert!(entity_number < 3);
-                match CONNECTED_DIM {
-                    0 => Ok(self.edges()[entity_number * 2..(entity_number + 1) * 2].to_vec()),
-                    1 => Ok(vec![entity_number]),
-                    2 => Ok(vec![0]),
-                    _ => Err(()),
-                }
-            }
-            2 => {
-                assert!(entity_number == 0);
-                match CONNECTED_DIM {
-                    0 => Ok(vec![0, 1, 2]),
-                    1 => Ok(vec![0, 1, 2]),
-                    2 => Ok(vec![0]),
-                    _ => Err(()),
-                }
-            }
-            _ => Err(()),
-        }
-    }
-}
 
 /// The reference quadrilateral
 struct Quadrilateral;
@@ -289,6 +214,7 @@ mod test {
         assert_eq!(c.vertex_count(), c.vertices().len() / c.dim());
         assert_eq!(c.edge_count(), c.edges().len() / 2);
         assert_eq!(c.face_count(), c.faces_nvertices().len());
+        // TODO: test connectivity
     }
 
     /*#[test]
