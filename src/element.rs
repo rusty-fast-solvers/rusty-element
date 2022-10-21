05e8ff19638cb1c4ce8e4c2f81e49c6976c03f73
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-10-21 13:58:21 +0100

    Add tabulate for Lagrange 1

--- src/element.rs
+++ src/element.rs
@@ -11,19 +11,82 @@ pub enum ElementFamily {
 }
 
 /// A finite element
-pub trait FiniteElement {
+pub trait FiniteElement: Sized {
     const VALUE_SIZE: usize;
     fn cell_type(&self) -> ReferenceCellType;
     fn degree(&self) -> usize;
     fn highest_degree(&self) -> usize;
     fn family(&self) -> ElementFamily;
+    fn dim(&self) -> usize;
     fn discontinuous(&self) -> bool;
 
     fn value_size(&self) -> usize {
         Self::VALUE_SIZE
     }
 
-    // fn tabulate(&self, points: &[f64], nderivs: usize) -> &[f64];
+    fn tabulate(&self, points: &[f64], nderivs: usize, data: &mut TabulatedData<Self>);
+}
+
+pub struct TabulatedData<'a, F: FiniteElement> {
+    data: Vec<f64>,
+    element: &'a F,
+    deriv_count: usize,
+    point_count: usize,
+    basis_count: usize,
+    value_size: usize,
+}
+
+impl<'a, F: FiniteElement> TabulatedData<'a, F> {
+    pub fn new(element: &'a F, nderivs: usize, npoints: usize) -> Self {
+        let deriv_count = (nderivs + 1) * (nderivs + 2) / 2; // 2D is hardcoded here
+        let point_count = npoints;
+        let basis_count = element.dim();
+        let value_size = element.value_size();
+        let data = vec![0.0; deriv_count * point_count * basis_count * value_size];
+        Self {
+            data,
+            element,
+            deriv_count,
+            point_count,
+            basis_count,
+            value_size,
+        }
+    }
+
+    pub fn get_mut(
+        &mut self,
+        deriv: usize,
+        point: usize,
+        basis: usize,
+        component: usize,
+    ) -> &mut f64 {
+        /// Debug here
+        let index = ((deriv * self.point_count + point) * self.basis_count + basis)
+            * self.value_size
+            + component;
+        self.data.get_mut(index).unwrap()
+    }
+
+    pub fn get(&mut self, deriv: usize, point: usize, basis: usize, component: usize) -> &f64 {
+        /// Debug here
+        let index = ((deriv * self.point_count + point) * self.basis_count + basis)
+            * self.value_size
+            + component;
+        self.data.get(index).unwrap()
+    }
+
+    pub fn deriv_count(&self) -> usize {
+        self.deriv_count
+    }
+    pub fn point_count(&self) -> usize {
+        self.point_count
+    }
+    pub fn basis_count(&self) -> usize {
+        self.basis_count
+    }
+    pub fn value_size(&self) -> usize {
+        self.value_size
+    }
 }
 
 #[cfg(test)]
--- src/element/lagrange.rs
+++ src/element/lagrange.rs
@@ -3,7 +3,7 @@
 use crate::cell::*;
 use crate::element::*;
 
-/// The reference interval
+/// Lagrange element
 pub struct LagrangeElement {
     pub celltype: ReferenceCellType,
     pub degree: usize,
@@ -25,5 +25,100 @@ impl FiniteElement for LagrangeElement {
     }
     fn discontinuous(&self) -> bool {
         false
+    }
+    fn dim(&self) -> usize {
+        unimplemented!("dim not yet implemented for this element");
+    }
+    fn tabulate(&self, points: &[f64], nderivs: usize, mut data: &mut TabulatedData<Self>) {
+        unimplemented!("tabulate not yet implemented for this element");
+    }
+}
+
+/// Degree 1 Lagrange element
+pub struct LagrangeElementDegree1 {
+    pub celltype: ReferenceCellType,
+}
+
+impl FiniteElement for LagrangeElementDegree1 {
+    const VALUE_SIZE: usize = 1;
+    fn cell_type(&self) -> ReferenceCellType {
+        self.celltype
+    }
+    fn degree(&self) -> usize {
+        1
+    }
+    fn highest_degree(&self) -> usize {
+        1
+    }
+    fn family(&self) -> ElementFamily {
+        ElementFamily::Lagrange
+    }
+    fn discontinuous(&self) -> bool {
+        false
+    }
+    fn dim(&self) -> usize {
+        3
+    }
+    fn tabulate(&self, points: &[f64], nderivs: usize, mut data: &mut TabulatedData<Self>) {
+        // Basis functions are 1-x-y, x, y
+        // derivative, point, basis fn index, value index
+        for deriv in 0..data.deriv_count() {
+            for pt in 0..data.point_count() {
+                if deriv == 0 {
+                    *data.get_mut(deriv, pt, 0, 0) = 1.0 - points[2 * pt] - points[2 * pt + 1];
+                    *data.get_mut(deriv, pt, 1, 0) = points[2 * pt];
+                    *data.get_mut(deriv, pt, 2, 0) = points[2 * pt + 1];
+                } else if deriv == 1 {
+                    *data.get_mut(deriv, pt, 0, 0) = -1.0;
+                    *data.get_mut(deriv, pt, 1, 0) = 1.0;
+                    *data.get_mut(deriv, pt, 2, 0) = 0.0;
+                } else if deriv == 2 {
+                    *data.get_mut(deriv, pt, 0, 0) = -1.0;
+                    *data.get_mut(deriv, pt, 1, 0) = 0.0;
+                    *data.get_mut(deriv, pt, 2, 0) = 1.0;
+                } else {
+                    for fun in 0..3 {
+                        *data.get_mut(deriv, pt, fun, 0) = 0.;
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::cell::*;
+    use crate::element::*;
+    use approx::*;
+
+    #[test]
+    fn test_lagrange_1() {
+        let e = LagrangeElementDegree1 {
+            celltype: ReferenceCellType::Triangle,
+        };
+        assert_eq!(e.value_size(), 1);
+        let mut data = TabulatedData::new(&e, 0, 6);
+        let points = vec![0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.5, 0.0, 0.0, 0.5, 0.5, 0.5];
+        e.tabulate(&points, 0, &mut data);
+
+        assert_relative_eq!(*data.get(0, 0, 0, 0), 1.0);
+        assert_relative_eq!(*data.get(0, 0, 1, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 0, 2, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 1, 0, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 1, 1, 0), 1.0);
+        assert_relative_eq!(*data.get(0, 1, 2, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 2, 0, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 2, 1, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 2, 2, 0), 1.0);
+        assert_relative_eq!(*data.get(0, 3, 0, 0), 0.5);
+        assert_relative_eq!(*data.get(0, 3, 1, 0), 0.5);
+        assert_relative_eq!(*data.get(0, 3, 2, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 4, 0, 0), 0.5);
+        assert_relative_eq!(*data.get(0, 4, 1, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 4, 2, 0), 0.5);
+        assert_relative_eq!(*data.get(0, 5, 0, 0), 0.0);
+        assert_relative_eq!(*data.get(0, 5, 1, 0), 0.5);
+        assert_relative_eq!(*data.get(0, 5, 2, 0), 0.5);
     }
 }
