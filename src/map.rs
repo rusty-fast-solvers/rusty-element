b8966d0177d645bf1c8b4e6fe9f7a8d093958029
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-11-03 16:15:15 +0000

    Add L2 piola maps

--- src/map.rs
+++ src/map.rs
@@ -248,6 +248,98 @@ pub fn covariant_piola_pull_back<'a, 'b, F: FiniteElement, F2: FiniteElement, C:
     }
 }
 
+pub fn l2_piola_push_forward<'a, 'b, F: FiniteElement, F2: FiniteElement, C: ReferenceCell>(
+    data: &mut TabulatedData<'a, F>,
+    points: &[f64],
+    geometry: &PhysicalCell<'b, F2, C>,
+) {
+    assert_eq!(data.deriv_count(), 1);
+
+    if geometry.tdim() == 2 && geometry.gdim() == 2 {
+        let gdim = geometry.gdim();
+        let npts = points.len() / gdim;
+        let geometry_npts = geometry.npts();
+        let nbasis = data.basis_count();
+
+        // TODO: get rid of memory assignment inside this function
+        let mut derivs = TabulatedData::new(geometry.coordinate_element(), 1, npts);
+        geometry
+            .coordinate_element()
+            .tabulate(&points, 1, &mut derivs);
+
+        let mut j = vec![0.0, 0.0, 0.0, 0.0];
+        let mut det_j = 0.0;
+
+        let mut temp_data = vec![0.0, 0.0];
+
+        for p in 0..npts {
+            j[0] = 0.0;
+            j[1] = 0.0;
+            j[2] = 0.0;
+            j[3] = 0.0;
+            for gp in 0..geometry_npts {
+                j[0] += derivs.get(1, p, gp, 0) * geometry.vertices()[gp * gdim];
+                j[1] += derivs.get(2, p, gp, 0) * geometry.vertices()[gp * gdim];
+                j[2] += derivs.get(1, p, gp, 0) * geometry.vertices()[gp * gdim + 1];
+                j[3] += derivs.get(2, p, gp, 0) * geometry.vertices()[gp * gdim + 1];
+            }
+            det_j = j[0] * j[3] - j[1] * j[2];
+
+            for i in 0..nbasis {
+                *data.get_mut(0, p, i, 0) *= det_j;
+            }
+        }
+    } else {
+        unimplemented!("push_forward not yet implemented for this element");
+    }
+}
+
+pub fn l2_piola_pull_back<'a, 'b, F: FiniteElement, F2: FiniteElement, C: ReferenceCell>(
+    data: &mut TabulatedData<'a, F>,
+    points: &[f64],
+    geometry: &PhysicalCell<'b, F2, C>,
+) {
+    assert_eq!(data.deriv_count(), 1);
+
+    if geometry.tdim() == 2 && geometry.gdim() == 2 {
+        let gdim = geometry.gdim();
+        let npts = points.len() / gdim;
+        let geometry_npts = geometry.npts();
+        let nbasis = data.basis_count();
+
+        // TODO: get rid of memory assignment inside this function
+        let mut derivs = TabulatedData::new(geometry.coordinate_element(), 1, npts);
+        geometry
+            .coordinate_element()
+            .tabulate(&points, 1, &mut derivs);
+
+        let mut j = vec![0.0, 0.0, 0.0, 0.0];
+        let mut det_j = 0.0;
+
+        let mut temp_data = vec![0.0, 0.0];
+
+        for p in 0..npts {
+            j[0] = 0.0;
+            j[1] = 0.0;
+            j[2] = 0.0;
+            j[3] = 0.0;
+            for gp in 0..geometry_npts {
+                j[0] += derivs.get(1, p, gp, 0) * geometry.vertices()[gp * gdim];
+                j[1] += derivs.get(2, p, gp, 0) * geometry.vertices()[gp * gdim];
+                j[2] += derivs.get(1, p, gp, 0) * geometry.vertices()[gp * gdim + 1];
+                j[3] += derivs.get(2, p, gp, 0) * geometry.vertices()[gp * gdim + 1];
+            }
+            det_j = j[0] * j[3] - j[1] * j[2];
+
+            for i in 0..nbasis {
+                *data.get_mut(0, p, i, 0) /= det_j;
+            }
+        }
+    } else {
+        unimplemented!("push_forward not yet implemented for this element");
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::cell::*;
@@ -358,5 +450,34 @@ mod test {
         assert_relative_eq!(*data.get(0, 0, 1, 1), 0.2);
         assert_relative_eq!(*data.get(0, 0, 2, 0), 0.1);
         assert_relative_eq!(*data.get(0, 0, 2, 1), 0.0);
+    }
+
+    #[test]
+    fn test_l2_piola() {
+        let e = LagrangeElementTriangleDegree1 {};
+        let mut data = TabulatedData::new(&e, 0, 1);
+
+        *data.get_mut(0, 0, 0, 0) = 0.5;
+        *data.get_mut(0, 0, 1, 0) = 0.4;
+        *data.get_mut(0, 0, 2, 0) = 0.3;
+
+        let coord_e = LagrangeElementTriangleDegree1 {};
+        let ref_cell = Triangle {};
+        let vertices = vec![0.0, 1.0, 1.0, 0.0, 2.0, 1.0];
+        let geometry = PhysicalCell::new(&ref_cell, &vertices, &coord_e, 2);
+
+        let pts = vec![0.3, 0.3];
+
+        l2_piola_push_forward(&mut data, &pts, &geometry);
+
+        assert_relative_eq!(*data.get(0, 0, 0, 0), 1.0);
+        assert_relative_eq!(*data.get(0, 0, 1, 0), 0.8);
+        assert_relative_eq!(*data.get(0, 0, 2, 0), 0.6);
+
+        l2_piola_pull_back(&mut data, &pts, &geometry);
+
+        assert_relative_eq!(*data.get(0, 0, 0, 0), 0.5);
+        assert_relative_eq!(*data.get(0, 0, 1, 0), 0.4);
+        assert_relative_eq!(*data.get(0, 0, 2, 0), 0.3);
     }
 }
